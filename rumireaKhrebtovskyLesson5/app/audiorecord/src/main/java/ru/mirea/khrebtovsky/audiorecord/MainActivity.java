ee8ddd465fec14a373c1d97be75831e02b054cfa
AlexHrebto <alexhrebto@yandex.ru>
2024-05-01 04:19:06 +0300

    lesson5 and mireaproject

---
+++ rumireaKhrebtovskyLesson5/app/audiorecord/src/main/java/ru/mirea/khrebtovsky/audiorecord/MainActivity.java
@@ -0,0 +1,121 @@
+package ru.mirea.khrebtovsky.audiorecord;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.IOException;
+import ru.mirea.khrebtovsky.audiorecord.databinding.ActivityMainBinding;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private static final int REQUEST_CODE_PERMISSION = 200;
+    private final String TAG = MainActivity.class.getSimpleName();
+    private boolean isStartRecording = true;
+    private boolean isStartPlaying = true;
+    private String recordFilePath;
+    private MediaRecorder recorder;
+    private MediaPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        checkPermissions();
+
+        Button recordButton = binding.recordButton;
+        Button playButton = binding.playButton;
+        playButton.setEnabled(false);
+
+        recordFilePath = new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC), "audiorecordtest.3gp").getAbsolutePath();
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isStartRecording) {
+                    recordButton.setText("Stop recording");
+                    playButton.setEnabled(false);
+                    startRecording();
+                } else {
+                    recordButton.setText("Start recording");
+                    playButton.setEnabled(true);
+                    stopRecording();
+                }
+                isStartRecording = !isStartRecording;
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isStartPlaying) {
+                    playButton.setText("Stop playing");
+                    recordButton.setEnabled(false);
+                    startPlaying();
+                } else {
+                    playButton.setText("Start playing");
+                    recordButton.setEnabled(true);
+                    stopPlaying();
+                }
+                isStartPlaying = !isStartPlaying;
+            }
+        });
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_CODE_PERMISSION);
+        }
+    }
+
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(recordFilePath);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(recordFilePath);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
+    }
+}
---
+++ rumireaKhrebtovskyLesson5/app/camera/src/main/java/ru/mirea/khrebtovsky/camera/MainActivity.java
@@ -0,0 +1,75 @@
+package ru.mirea.khrebtovsky.camera;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+
+import ru.mirea.khrebtovsky.camera.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_PERMISSION = 100;
+    private static final int CAMERA_REQUEST = 0;
+    private boolean isWork = false;
+    private Uri imageUri;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSION);
+        } else {
+            isWork = true;
+        }
+
+        binding.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (isWork) {
+                    try {
+                        File photoFile = createImageFile();
+                        String authorities = getApplicationContext().getPackageName() + ".fileprovider";
+                        imageUri = FileProvider.getUriForFile(MainActivity.this, authorities, photoFile);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+
+    private File createImageFile() throws IOException {
+
+        return File.createTempFile("image", ".jpg", getExternalFilesDir(null));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            binding.imageView.setImageURI(imageUri);
+        }
+    }
+}
